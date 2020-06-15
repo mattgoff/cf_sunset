2f91198b84a9127c4aefb049778b2963ca45a8da
Matt Goff <mgoff72@gmail.com>
2020-06-15 16:54:25 -0700

    initialCommit

---
+++ Models/SkylineModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace cf_Sunset.Models
+{
+  public class SkylineModel
+  {
+    [Required]
+    public string skylineString {get; set;}
+  }
+
+}
+
