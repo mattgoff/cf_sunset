9e7e709531b69417f10e573acfdab2497c195538
Matt Goff <mgoff72@gmail.com>
2020-06-17 07:50:56 -0700

    cleanup

--- Models/SkylineModel.cs
+++ Models/SkylineModel.cs
@@ -6,8 +6,8 @@ namespace cf_Sunset.Models
   public class SkylineModel
   {
     [Required]
-    [DataType(DataType.Text,ErrorMessage="Building Height data is invalid 1.")]
-    [RegularExpression(@"[-+]?(?:[0-9]+,)*[0-9]+(?:\.[0-9]+)?",ErrorMessage="Building Height data is invalid. It must be in the format #,#,#,#")]
+    [DataType(DataType.Text,ErrorMessage="Building Height data is invalid.")]
+    [RegularExpression(@"[-+]?(?:[0-9]+,)*[0-9]+(?:\.[0-9]+)?",ErrorMessage="Building Height format is invalid. It must be in the format #,#,#,#")]
     public string skylineString {get; set;}
   }
 
